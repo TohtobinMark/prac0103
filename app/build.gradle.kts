3f148aa8e12f0f61d13dd6a4ff3a667a89b9c099
student-pro <marktokhtobin@gmail.com>
2026-02-26 17:43:34 +0700

    Ошибка

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,6 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
